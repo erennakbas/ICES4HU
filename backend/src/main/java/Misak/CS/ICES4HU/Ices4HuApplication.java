55bf411f12203f6b91c5904ab1f1de5bfd6fcd98
erennakbas <ahmet.eren4219@hotmail.com>
2023-03-22 14:00:20 +0300

    initial commit

---
+++ backend/src/main/java/Misak/CS/ICES4HU/Ices4HuApplication.java
@@ -0,0 +1,13 @@
+package Misak.CS.ICES4HU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ices4HuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ices4HuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Misak/CS/ICES4HU/Ices4HuApplicationTests.java
@@ -0,0 +1,13 @@
+package Misak.CS.ICES4HU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ices4HuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
