be4534af893d2e28829ad9c056836d105ec46b5d
theroxz <ahmet.eren4219@hotmail.com>
2023-05-01 11:54:10 +0300

    Bugfix and dependency fix

--- backend/src/main/java/Misak/CS/ICES4HU/EnrollmentRequest/Repository/EnrollmentRequestRepository.java
+++ backend/src/main/java/Misak/CS/ICES4HU/EnrollmentRequest/Repository/EnrollmentRequestRepository.java
@@ -1,6 +1,7 @@
 package Misak.CS.ICES4HU.EnrollmentRequest.Repository;
 
+import Misak.CS.ICES4HU.EnrollmentRequest.Entity.EnrollmentRequestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EnrollmentRequestRepository extends JpaRepository<EnrollmentRequestRepository, Long> {
+public interface EnrollmentRequestRepository extends JpaRepository<EnrollmentRequestEntity, Long> {
 }
