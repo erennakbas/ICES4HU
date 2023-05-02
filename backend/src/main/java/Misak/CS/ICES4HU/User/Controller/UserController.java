8e46ba03eeeb0dbec6c58e5648f1d1329f661eed
b21945944 <b21945944@cs.hacettepe.edu.tr>
2023-05-02 22:32:32 +0300

    Revert "Merge branch 'main' of https://github.com/erennakbas/Misak-CS into Login"
    This reverts commit bac2b7f8f57b2176062cb6403ea10177148c9325, reversing
    changes made to cf33120a05e97a57960e2be992cbded3078cc853.

--- backend/src/main/java/Misak/CS/ICES4HU/User/Controller/UserController.java
+++ backend/src/main/java/Misak/CS/ICES4HU/User/Controller/UserController.java
@@ -2,14 +2,10 @@ package Misak.CS.ICES4HU.User.Controller;
 
 import Misak.CS.ICES4HU.User.Entity.UserEntity;
 import Misak.CS.ICES4HU.User.Service.UserService;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
@@ -21,14 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping(path="/api")
 public class UserController {
-
-    @Autowired
     private UserService userService;
-
-    @Bean
-    public void initUsers(){
-        userService.initUsers();
-    }
     @GetMapping(path="/users")
     public ResponseEntity<List<UserEntity>> getUsers(){
         return ResponseEntity.ok(userService.getUsers());
@@ -36,17 +25,6 @@ public class UserController {
     @PostMapping(path="/users")
     public ResponseEntity<UserEntity> saveUser(@RequestBody UserEntity user){
         return ResponseEntity.ok(userService.saveUser(user));
-    }
-
-    @PostMapping(path="/login")
-    public ResponseEntity<UserEntity> login(@Valid @RequestParam String schoolId,@Valid @RequestParam String password){
-        UserEntity userEntity = userService.login(schoolId, password);
-        if (userEntity != null){
-            System.out.println("User logged in");
-            return ResponseEntity.ok(userEntity);
-        }
-        System.out.println("User not logged in");
-        return new ResponseEntity<>(null , HttpStatus.BAD_REQUEST);
     }
 
 }
