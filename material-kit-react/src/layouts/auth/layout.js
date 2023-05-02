8eca159c73baa5b1a57534b01d84036c4c34fa96
b21946735 <b21946735@cs.hacettepe.edu.tr>
2023-05-02 22:53:35 +0300

    Revert "Login"
    This reverts commit cf33120a05e97a57960e2be992cbded3078cc853.

--- material-kit-react/src/layouts/auth/layout.js
+++ material-kit-react/src/layouts/auth/layout.js
@@ -1,7 +1,7 @@
-import PropTypes from "prop-types";
-import NextLink from "next/link";
-import { Box, Typography, Unstable_Grid2 as Grid } from "@mui/material";
-import { Logo } from "src/components/logo";
+import PropTypes from 'prop-types';
+import NextLink from 'next/link';
+import { Box, Typography, Unstable_Grid2 as Grid } from '@mui/material';
+import { Logo } from 'src/components/logo';
 
 // TODO: Change subtitle text
 
@@ -12,19 +12,22 @@ export const Layout = (props) => {
     <Box
       component="main"
       sx={{
-        display: "flex",
-        flex: "1 1 auto",
+        display: 'flex',
+        flex: '1 1 auto'
       }}
     >
-      <Grid container sx={{ flex: "1 1 auto" }}>
+      <Grid
+        container
+        sx={{ flex: '1 1 auto' }}
+      >
         <Grid
           xs={12}
           lg={6}
           sx={{
-            backgroundColor: "background.paper",
-            display: "flex",
-            flexDirection: "column",
-            position: "relative",
+            backgroundColor: 'background.paper',
+            display: 'flex',
+            flexDirection: 'column',
+            position: 'relative'
           }}
         >
           <Box
@@ -32,20 +35,23 @@ export const Layout = (props) => {
             sx={{
               left: 0,
               p: 3,
-              position: "fixed",
+              position: 'fixed',
               top: 0,
-              width: "100%",
+              width: '100%'
             }}
           >
             <Box
               component={NextLink}
               href="/"
               sx={{
-                display: "inline-flex",
+                display: 'inline-flex',
                 height: 32,
-                width: 32,
+                width: 32
               }}
-            ></Box>
+            >
+              
+
+            </Box>
           </Box>
           {children}
         </Grid>
@@ -53,14 +59,14 @@ export const Layout = (props) => {
           xs={12}
           lg={6}
           sx={{
-            alignItems: "center",
-            background: "radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)",
-            color: "white",
-            display: "flex",
-            justifyContent: "center",
-            "& img": {
-              maxWidth: "100%",
-            },
+            alignItems: 'center',
+            background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
+            color: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            '& img': {
+              maxWidth: '100%'
+            }
           }}
         >
           <Box sx={{ p: 3 }}>
@@ -68,24 +74,32 @@ export const Layout = (props) => {
               align="center"
               color="inherit"
               sx={{
-                fontSize: "24px",
-                lineHeight: "32px",
-                mb: 1,
-                alignItems: "center",
-                display: "flex",
-                justifyContent: "center",
+                fontSize: '24px',
+                lineHeight: '32px',
+                mb: 1
               }}
               variant="h1"
             >
-              Welcome to{" "}
-              <Box component="a" sx={{ color: "#15B79E" }} target="_blank">
+              Welcome to{' '}
+              <Box
+                component="a"
+                sx={{ color: '#15B79E' }}
+                target="_blank"
+              >
                 ICS4HU System
               </Box>
             </Typography>
-            <Typography align="center" sx={{ mb: 3 }} variant="subtitle1">
+            <Typography
+              align="center"
+              sx={{ mb: 3 }}
+              variant="subtitle1"
+            >
               A professional registeration system for Hacettepe University.
             </Typography>
-            <img alt="" src="/assets/hacettepex.png" />
+            <img
+              alt=""
+              src="/assets/hacettepex.png"
+            />
           </Box>
         </Grid>
       </Grid>
@@ -94,5 +108,5 @@ export const Layout = (props) => {
 };
 
 Layout.prototypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
--- material-kit-react/src/pages/auth/login.js
+++ material-kit-react/src/pages/auth/login.js
@@ -1,9 +1,9 @@
-import { useCallback, useState } from "react";
-import Head from "next/head";
-import NextLink from "next/link";
-import { useRouter } from "next/navigation";
-import { useFormik } from "formik";
-import * as Yup from "yup";
+import { useCallback, useState } from 'react';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import {
   Alert,
   Box,
@@ -14,77 +14,104 @@ import {
   Tab,
   Tabs,
   TextField,
-  Typography,
-} from "@mui/material";
-import { useAuth } from "src/hooks/use-auth";
-import { Layout as AuthLayout } from "src/layouts/auth/layout";
+  Typography
+} from '@mui/material';
+import { useAuth } from 'src/hooks/use-auth';
+import { Layout as AuthLayout } from 'src/layouts/auth/layout';
 
 const Page = () => {
   const router = useRouter();
   const auth = useAuth();
-  const [method, setMethod] = useState("email");
+  const [method, setMethod] = useState('email');
   const formik = useFormik({
     initialValues: {
-      id: "b21945944",
-      password: "Password123!",
-      submit: null,
+      email: 'CSadmin@hotmail.com',
+      password: 'Password123!',
+      submit: null
     },
     //email yazılacak username yerine
     validationSchema: Yup.object({
-      id: Yup.string().max(255).required("User Id is required"),
-      password: Yup.string().max(255).required("Password is required"),
+      username: Yup
+        .string()
+        .email('Must be a valid email')
+        .max(255)
+        .required('Email is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required('Password is required')
     }),
     onSubmit: async (values, helpers) => {
       try {
-        await auth.signIn(values.id, values.password);
-        router.push("/");
+        await auth.signIn(values.email, values.password);
+        router.push('/');
       } catch (err) {
         helpers.setStatus({ success: false });
         helpers.setErrors({ submit: err.message });
         helpers.setSubmitting(false);
       }
-    },
+    }
   });
 
-  const handleMethodChange = useCallback((event, value) => {
-    setMethod(value);
-  }, []);
+  const handleMethodChange = useCallback(
+    (event, value) => {
+      setMethod(value);
+    },
+    []
+  );
 
-  const handleSkip = useCallback(() => {
-    auth.skip();
-    router.push("/");
-  }, [auth, router]);
-  const handleForgotPassword = useCallback(() => {
-    router.push("/forgot_password");
-  }, [router]);
+  const handleSkip = useCallback(
+    () => {
+      auth.skip();
+      router.push('/');
+    },
+    [auth, router]
+  );
+  const handleForgotPassword = useCallback(
+    () => {
+      router.push('/forgot_password');
+    },
+    [router]
+  );
 
   return (
     <>
       <Head>
-        <title>ICS4HU | Login</title>
+        <title>
+          Login | Devias Kit
+        </title>
       </Head>
       <Box
         sx={{
-          backgroundColor: "background.paper",
-          flex: "1 1 auto",
-          alignItems: "center",
-          display: "flex",
-          justifyContent: "center",
+          backgroundColor: 'background.paper',
+          flex: '1 1 auto',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center'
         }}
       >
         <Box
           sx={{
             maxWidth: 550,
             px: 3,
-            py: "100px",
-            width: "100%",
+            py: '100px',
+            width: '100%'
           }}
         >
           <div>
-            <Stack spacing={1} sx={{ mb: 3 }}>
-              <Typography variant="h4">Login</Typography>
-              <Typography color="text.secondary" variant="body2">
-                Don&apos;t have an account? &nbsp;
+            <Stack
+              spacing={1}
+              sx={{ mb: 3 }}
+            >
+              <Typography variant="h4">
+                Login
+              </Typography>
+              <Typography
+                color="text.secondary"
+                variant="body2"
+              >
+                Don&apos;t have an account?
+                &nbsp;
                 <Link
                   component={NextLink}
                   href="/auth/register"
@@ -95,49 +122,158 @@ const Page = () => {
                 </Link>
               </Typography>
             </Stack>
-            <form noValidate onSubmit={formik.handleSubmit}>
-              <Stack spacing={3}>
-                <TextField
-                  error={!!(formik.touched.id && formik.errors.id)}
+            <Tabs
+              onChange={handleMethodChange}
+              sx={{ mb: 3 }}
+              value={method}
+            >
+              <Tab
+                label="Username"
+                value="username"
+              />
+              <Tab
+                label="Email"
+                value="email"
+              />
+            </Tabs>
+            {method === 'email' && (
+              <form
+                noValidate
+                onSubmit={formik.handleSubmit}
+              >
+                <Stack spacing={3}>
+                  <TextField
+                    error={!!(formik.touched.email && formik.errors.email)}
+                    fullWidth
+                    helperText={formik.touched.email && formik.errors.email}
+                    label="Email Address"
+                    name="email"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="email"
+                    value={formik.values.email}
+                  />
+                  <TextField
+                    error={!!(formik.touched.password && formik.errors.password)}
+                    fullWidth
+                    helperText={formik.touched.password && formik.errors.password}
+                    label="Password"
+                    name="password"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="password"
+                    value={formik.values.password}
+                  />
+                </Stack>
+                <FormHelperText sx={{ mt: 1 }}>
+                  Optionally you can skip.
+                </FormHelperText>
+                {formik.errors.submit && (
+                  <Typography
+                    color="error"
+                    sx={{ mt: 3 }}
+                    variant="body2"
+                  >
+                    {formik.errors.submit}
+                  </Typography>
+                )}
+                <Button
                   fullWidth
-                  helperText={formik.touched.id && formik.errors.id}
-                  label="UserId"
-                  name="id"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  type="id"
-                  value={formik.values.id}
-                />
-                <TextField
-                  error={!!(formik.touched.password && formik.errors.password)}
+                  size="large"
+                  sx={{ mt: 3 }}
+                  type="submit"
+                  variant="contained"
+                >
+                  Continue
+                </Button>
+                <Button
                   fullWidth
-                  helperText={formik.touched.password && formik.errors.password}
-                  label="Password"
-                  name="password"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  type="password"
-                  value={formik.values.password}
-                />
-              </Stack>
-              <FormHelperText sx={{ mt: 1 }}></FormHelperText>
-              {formik.errors.submit && (
-                <Typography color="error" sx={{ mt: 3 }} variant="body2">
-                  {formik.errors.submit}
-                </Typography>
-              )}
-              <Button fullWidth size="large" sx={{ mt: 3 }} type="submit" variant="contained">
-                Continue
-              </Button>
-              <Button fullWidth size="large" sx={{ mt: 3 }} onClick={handleForgotPassword}>
-                Forgot your password?
-              </Button>
-              <Alert color="primary" severity="info" sx={{ mt: 3 }}>
-                <div>
-                  You can use <b>CSadmin</b> and password <b>admin</b>
-                </div>
-              </Alert>
-            </form>
+                  size="large"
+                  sx={{ mt: 3 }}
+                  onClick={handleSkip}
+                >
+                  Skip authentication
+                </Button>
+                <Alert
+                  color="primary"
+                  severity="info"
+                  sx={{ mt: 3 }}
+                >
+                  <div>
+                    You can use <b>demo@devias.io</b> and password <b>Password123!</b>
+                  </div>
+                </Alert>
+              </form>
+            )}
+            {method === 'username' && (
+              <form
+                noValidate
+                onSubmit={formik.handleSubmit}
+              >
+                <Stack spacing={3}>
+                  <TextField
+                    error={!!(formik.touched.email && formik.errors.email)}
+                    fullWidth
+                    helperText={formik.touched.email && formik.errors.email}
+                    label="Username"
+                    name="email"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="email"
+                    value={formik.values.email}
+                  />
+                  <TextField
+                    error={!!(formik.touched.password && formik.errors.password)}
+                    fullWidth
+                    helperText={formik.touched.password && formik.errors.password}
+                    label="Password"
+                    name="password"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="password"
+                    value={formik.values.password}
+                  />
+                </Stack>
+                <FormHelperText sx={{ mt: 1 }}>
+                  
+                </FormHelperText>
+                {formik.errors.submit && (
+                  <Typography
+                    color="error"
+                    sx={{ mt: 3 }}
+                    variant="body2"
+                  >
+                    {formik.errors.submit}
+                  </Typography>
+                )}
+                <Button
+                  fullWidth
+                  size="large"
+                  sx={{ mt: 3 }}
+                  type="submit"
+                  variant="contained"
+                >
+                  Continue
+                </Button>
+                <Button
+                  fullWidth
+                  size="large"
+                  sx={{ mt: 3 }}
+                  onClick={handleForgotPassword}
+                >
+                  Forgot your password?
+                </Button>
+                <Alert
+                  color="primary"
+                  severity="info"
+                  sx={{ mt: 3 }}
+                >
+                  <div>
+                    You can use <b>CSadmin</b> and password <b>admin</b>
+                  </div>
+                </Alert>
+              </form>
+            )}
           </div>
         </Box>
       </Box>
@@ -145,6 +281,10 @@ const Page = () => {
   );
 };
 
-Page.getLayout = (page) => <AuthLayout>{page}</AuthLayout>;
+Page.getLayout = (page) => (
+  <AuthLayout>
+    {page}
+  </AuthLayout>
+);
 
 export default Page;
